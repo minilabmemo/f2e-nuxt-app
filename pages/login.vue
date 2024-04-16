ff6636ceaff2e74734d5a779e4a1696eae0ca1b4
minilabmemo <minilabmemo@gmail.com>
2024-04-16 10:34:24 +0800

    Add: Logout

--- pages/login.vue
+++ pages/login.vue
@@ -5,12 +5,19 @@
       <ClientOnly>
         <GoogleLogin :callback="callback" />
       </ClientOnly>
+
+      <button type="button" @click="Logout">登出</button>
     </div>
   </div>
 </template>
 <script setup>
+import {googleLogout} from 'vue3-google-login';
 const callback = (response) => {
   console.log(response);
   // {clientId: 'xxx', credential: 'xxx', select_by: 'btn_confirm'} //TODO 接著就可以拿到使用者資訊
 };
+function Logout() {
+  console.log('Logout'); // FIXME 按下去之後，我的所有 google 帳號就會被登出惹～～～？！！
+  googleLogout();
+}
 </script>
